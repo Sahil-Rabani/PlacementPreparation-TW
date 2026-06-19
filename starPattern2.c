05d62ed7fde192c0a9b893cd7347da2a0e45c143
Sahil Rabani <sahilrabani3@gmail.com>
2026-06-19 19:52:53 +0530

    Add files via upload

---
+++ starPattern2.c
@@ -0,0 +1,50 @@
+// 9. Print a Centered Pyramid of Stars
+// Output:
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+// 10. Print Stars and Spaces Alternating (Stars and Blank Spaces) (b = blank space here)
+// Output:
+// bbbb*
+// bbb*b*
+// bb*b*b*
+// b*b*b*b*
+// *b*b*b*b*
+
+// 11. Print Numbers in an Increasing Sequence (1, 12, 123, 1234, 12345)
+// Output:
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// 12. Print Repeated Numbers per Row (Same Number Repeated)
+// Output:
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+// 13. Print Numbers in a Continuous Triangle Pattern
+// Output:
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// 11 12 13 14 15
+
+// 14. Print Numbers in Floyd's Triangle Style
+// Output:
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 0
+// 1 2 3 4 5
+// 5 7 8 9 0 1
+// 2 3 4 5 6 7 8
+
